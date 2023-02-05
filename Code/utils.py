410c876c1e00fac0e2f06e5d45b93d9887100281
Nero <1600959@uab.cat>
2023-02-05 10:54:42 +0100

    Refactor utils functions into classes. main.py currently not functional

--- Code/utils.py
+++ Code/utils.py
@@ -1,26 +1,49 @@
 import numpy as np
 
-def rotate(v, b):
-    ux = v[0]*np.cos(b)-v[1]*np.sin(b)
-    uy = v[1]*np.cos(b)+v[0]*np.sin(b)
-    return [ux, uy, 0]
+class Point:
+    def __init__(self, x, y, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.c = [x,y,z]
+    def get(self):
+        return self.c
 
-def vbtw2(p, q):
-    v = [q[i]-p[i] for i in range(3)]
-    return v
+class Segment:
+    def __init__(self, a, b, color):
+        self.a = a
+        self.b = b
+        self.col = color
 
-def norma(v):
-    return np.sqrt(np.sum([vi*vi for vi in v]))
+class Vector:
+    def __init__(self, pt, og=Point(0,0)):
+        self.x = pt.x - og.x
+        self.y = pt.y - og.y
+        self.z = pt.z - og.z
+        self.c = [self.x, self.y, self.z]
+    def get(self):
+        return self.c
+    def norma(self):
+        return np.sqrt(np.sum([vi*vi for vi in self.c]))
+    def norml(self):
+        n = self.norma()
+        v = [round(vi/n,5) for vi in self.c]
+        return Vector(Point(v[0],v[1],v[2]))
+    def rotate(self, alpha):
+        ux = self.x*np.cos(alpha)-self.y*np.sin(alpha)
+        uy = self.y*np.cos(alpha)+self.x*np.sin(alpha)
+        return Vector(Point(ux, uy, self.z))
+    def pprod(self, other):
+        v = self.c
+        u = other.c
+        l = np.sum([v[i]*u[i] for i in range(3)])
+        return l
+    def xprod(self, other):
+        v = self.c
+        u = other.c
+        w = [v[1]*u[2]-v[2]*u[1],v[2]*u[0]-v[0]*u[2],v[0]*u[1]-v[1]*u[0]]
+        return Vector(Point(w[0],w[1],w[2]))
 
-def norml(v):
-    n = norma(v)
-    v = [round(vi/n,5) for vi in v]
-    return v
-
-def xprod(v, u):
-    w = [v[1]*u[2]-v[2]*u[1],v[2]*u[0]-v[0]*u[2],v[0]*u[1]-v[1]*u[0]]
-    w = [round(wi,5) for wi in w]
-    return w
-
+#should we define a raycast class?
 def rycst(og, vd, mu):
     return np.add(og, [vdi*mu for vdi in vd])
